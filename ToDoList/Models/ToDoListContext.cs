a967ee0ea54ddf39ac876a1925c7a8338b7174f5
Erik Igens and Jessica Munoz <jessica@>
2019-08-07 11:45:52 -0700

    Create categories controller, with edit and delete functionality through Entity.

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,7 +4,8 @@ namespace ToDoList.Models
 {
     public class ToDoListContext : DbContext
     {
-        public DbSet<Item> Items { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
         public ToDoListContext(DbContextOptions options) : base(options) { }
     }
 }
