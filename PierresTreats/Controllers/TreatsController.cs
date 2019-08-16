b921a7094177eb687798501522e48997699a5bdd
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-08-16 15:24:40 -0700

    It will migrate, creat new treats

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -1,76 +1,83 @@
-// using Microsoft.AspNetCore.Mvc;
-// using PierresTreats.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// namespace PierresTreats.Controllers
-// {
-//     public class TreatsController : Controller
-//     {
-//         private readonly PierresTreatsContext _db;
+namespace PierresTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
 
-//         public TreatsController(PierresTreatsContext db)
-//         {
-//             _db = db;
-//         }
+    public TreatsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
 
 
-//         public ActionResult Index()
-//         {
-//             List<Treat> model = _db.Treats.Include(treats => treats.Flavor).ToList();
-//             return View(model);
-//         }
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
 
-//         public ActionResult Create()
-//         {
-//             ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
-//             return View();
-//         }
+            public ActionResult Create()
+            {
+                ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+                return View();
+            }
 
-//         [HttpPost]
-//         public ActionResult Create(Treat treat)
-//         {
-//             _db.Treats.Add(treat);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+            [HttpPost]
+            public ActionResult Create(Treat treat, int FlavorId)
+            {
+                _db.Treats.Add(treat);
+                if (FlavorId != 0)
+                {
+                    _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId});
+                }
 
-//         public ActionResult Details(int id)
-//         {
-//             Treat thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
-//             return View(thisTreat);
-//         }
-//         public ActionResult Edit(int id)
-//         {
-//             var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
-//             ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
-//             return View(thisTreat);
-//         }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-//         [HttpPost]
-//         public ActionResult Edit(Treat treat)
-//         {
-//             _db.Entry(treat).State = EntityState.Modified;
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+        .Include(item => item.Flavors)
+        .ThenInclude(join => join.Flavor)
+        .FirstOrDefault(treats => treats.TreatId == id);
+      return View(thisTreat);
+    }
+    // public ActionResult Edit(int id)
+    // {
+    //     var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+    //     ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+    //     return View(thisTreat);
+    // }
 
-//         public ActionResult Delete(int id)
-//         {
-//             var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
-//             return View(thisTreat);
-//         }
+    //         [HttpPost]
+    //         public ActionResult Edit(Treat treat)
+    //         {
+    //             _db.Entry(treat).State = EntityState.Modified;
+    //             _db.SaveChanges();
+    //             return RedirectToAction("Index");
+    //         }
 
-//         [HttpPost, ActionName("Delete")]
-//         public ActionResult DeleteConfirmed(int id)
-//         {
-//             var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
-//             _db.Treats.Remove(thisTreat);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+    //         public ActionResult Delete(int id)
+    //         {
+    //             var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+    //             return View(thisTreat);
+    //         }
 
-//     }
-// }
+    //         [HttpPost, ActionName("Delete")]
+    //         public ActionResult DeleteConfirmed(int id)
+    //         {
+    //             var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+    //             _db.Treats.Remove(thisTreat);
+    //             _db.SaveChanges();
+    //             return RedirectToAction("Index");
+    //         }
+
+  }
+}
